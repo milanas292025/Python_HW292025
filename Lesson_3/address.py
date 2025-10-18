cd6efb8b3ff87770fb17c46084fe3bacc5610786
milanas292025 <milanas292025@gmail.com>
2025-10-18 17:26:14 +0500

    lesson3

---
+++ Lesson_3/address.py
@@ -0,0 +1,9 @@
+class Address:
+
+    def __init__(self, index, city, street, house, flat):
+         self.index = index
+         self.city = city
+         self.street = street
+         self.house = house
+         self.flat = flat
+
